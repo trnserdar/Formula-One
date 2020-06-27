050261e55b7788058875dc10b8a90ad542493dae
Serdar Turan <turan.serdar@consultant.turkcell.com.tr>
2020-06-27 12:29:22 +0300

    Initial Commit

---
+++ Shared/FormulaOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulaOneApp.swift
+//  Shared
+//
+//  Created by Serdar Turan on 27.06.2020.
+//
+
+import SwiftUI
+
+@main
+struct FormulaOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
